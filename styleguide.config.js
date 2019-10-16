a9a51d224f4f6767f0c5f2f90ffba9627442b9a9
gianfaye <git@gianfaye.com>
2019-10-16 16:33:32 +0800

    Initial commit

---
+++ styleguide.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    }
+  },
+  title: "Blacbox React Components Library",
+  styleguideDir: "dist-docs"
+};
